2187ccbcf7e4a6c3a4a5fe33e47e0ec07dec13ea
pyowonsik <qqrtyu@gmail.com>
2022-03-13 23:07:29 +0900

    SpringPractice

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 // Impl의 구현 객체 생성 , 생성자를 통해서 주입 , Impl이 실행에만 집중할수 있게 함 (연결 역할)
 // 사요영역인 Impl을 변경하지 않아도 됨.
+// appConfig.xml 과 비교
 @Configuration
 public class AppConfig {
 
---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,26 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.support.GenericXmlContextLoader;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class XmlAppContext  {
+
+
+    @Test
+    void XmlAppContext(){
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+
+
+
+
+
+}
